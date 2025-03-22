1ebcf8b07b12711c60e3a7106d943fe40a34fd19
Abraão Freitas Medeiros <abraaofmedeiros@sempreceub.com>
2025-03-22 11:47:45 -0300

    Update main.py
    Foram adicionados comentários nas funções display_menu.

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from NotLogged import *
 print("Bem Vindo ao gestor de despesas pessoais!")
 
 def display_menu():
+    """Função que apresenta o menu de opções. Ela irá verificar se tem um usuário logado, e apresentar o menu de opções adequado"""
     if app.get_active_user() == None:
         display_logged_out_menu()
     else:
         display_logged_in_menu()
 
+# A aplicação irá sempre estar executando a função de mostrar o menu.
 while True:
     display_menu()
